0d49831f8e4f9d5fd428c70ab61e8fefc9b4072c
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-03-03 21:22:37 +0500

    - Added more tests for users's endpoints
    =================================================
    - Добавлено больше тестов для users endpoint

--- test_app/test_main.py
+++ test_app/test_main.py
@@ -11,18 +11,36 @@ def test_get_user():
     assert response.json() == {'users': []}
 
 
-def test_get_post_del_user():
-    user = {"first_name": "Ivanov", "last_name": "Ivan", "email": "user@example.com", "age": 15}
+def test_post_user():
+    user = {"first_name": "Ivan", "last_name": "Ivanov", "email": "user@example.com", "age": 15}
     response = client.post("/users/add/", json=user)
     assert response.status_code == 200
-    assert response.json() == {"message": "User Ivanov Ivan with email user@example.com added"}
+    assert response.json() == {"message": "User Ivan Ivanov with email user@example.com added"}
 
 
 def test_del_user():
-    user = {"first_name": "Ivanov", "last_name": "Ivan", "email": "user@example.com", "age": 15}
+    user = {"first_name": "Ivan", "last_name": "Ivanov", "email": "user@example.com", "age": 15}
     response = client.request("DELETE", "/users/del/", json=user)
     assert response.status_code == 200
-    assert response.json() == {"message": "User Ivanov Ivan with email user@example.com deleted"}
+    assert response.json() == {"message": "User Ivan Ivanov with email user@example.com deleted"}
     response = client.get("/users/")
     assert response.status_code == 200
     assert response.json() == {'users': []}
+
+def test_fail_post_user():
+    user = {"first_name": "I", "last_name": "Ivanov", "email": "user@example.com", "age": 15}
+    response = client.post("/users/add/", json=user)
+    assert response.status_code == 422
+    assert (response.json()["detail"][0]["msg"]) == 'String should have at least 2 characters'
+    user = {"first_name": "Ivan", "last_name": "I", "email": "user@example.com", "age": 15}
+    response = client.post("/users/add/", json=user)
+    assert response.status_code == 422
+    assert (response.json()["detail"][0]["msg"]) == 'String should have at least 2 characters'
+    user = {"first_name": "Ivan", "last_name": "Ivanov", "email": "user@examplecom", "age": 15}
+    response = client.post("/users/add/", json=user)
+    assert response.status_code == 422
+    assert (response.json()["detail"][0]["msg"]) == 'value is not a valid email address: The part after the @-sign is not valid. It should have a period.'
+    user = {"first_name": "Ivan", "last_name": "Ivanov", "email": "user@example.com", "age": 1}
+    response = client.post("/users/add/", json=user)
+    assert response.status_code == 422
+    assert (response.json()["detail"][0]["msg"]) == 'Input should be greater than or equal to 10'
